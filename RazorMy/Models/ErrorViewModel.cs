a751c45e509bf8f154518a96c748dc3e64ea4b03
БахытжановА <@206-4>
2024-01-31 19:11:24 +0600

    Добавьте файлы проекта.

---
+++ RazorMy/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RazorMy.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
